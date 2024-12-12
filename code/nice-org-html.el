2ccdcdd60e93318b27154646440056d43e566e80
ewantown <etown@duck.com>
2024-12-12 04:08:06 -0800

    Generalized optional bullets.

--- code/nice-org-html.el
+++ code/nice-org-html.el
@@ -81,7 +81,9 @@
   "Default nice HTML page view mode ((quote light) or (quote dark)).")
 
 (defvar nice-org-html-headline-bullets nil
-  "If non-nil, headlines are prefixed with org-superstar style bullets.")
+  "If non-nil, headlines (up to level 3) are prefixed with bullets.
+If non-nil but not a plist of bullets, e.g. t, default bullets are used.
+Else, bullets are strings specified by plist of form '(:h1 x :h2 y :h3 z).")
 
 ;; Optional
 (defvar nice-org-html-header ""
@@ -197,10 +199,24 @@
    "<!--/*--><![CDATA[/*><!--*/\n"
    (with-temp-buffer
      (insert-file-contents nice-org-html--base-css)
-     (when nice-org-html-headline-bullets
-       (insert (concat "h2::before { content: '◉ '; }\n"
-		       "h3::before { content: '✸ '; }\n"
-		       "h4::before { content: '▷ '; }\n")))
+     (when-let* ((bulletvar nice-org-html-headline-bullets)
+		 (bullets
+		  (if (and (listp bulletvar)
+			   (--all?
+			    (and (plist-member bulletvar it)
+				 (stringp (plist-get bulletvar it)))
+			    '(:h1 :h2 :h3)))
+		      bulletvar
+		    '(:h1 "◉" :h2 "✸" :h3 "▷")))
+		 (mkcss
+		  (lambda (n)
+		    (format (concat "h%d::before { content: '%s';\n"
+				    "margin-right: calc(%d * 0.2em);\n}\n")
+			    (+ 1 n) ;; HTML h1 reserved for title
+			    (plist-get bullets (intern (format ":h%d" n)))
+			    (+ 1 n)))))
+       (insert (--reduce-r-from (concat it acc) ""
+				(--map (funcall mkcss it) '(1 2 3)))))
      (when (and (not (equal "" nice-org-html-css))
 		(file-exists-p nice-org-html-css))
        (insert-file-contents nice-org-html-css))
@@ -270,6 +286,7 @@
 	      (val (nice-org-html--get-hex-val str)))
 	(delete-region beg end)
 	(insert val)))
+    (goto-char (point-max))
     ;; restore prior theme configuration
     (unless (-contains? initial-themes nice-org-html--temp-theme)
       (disable-theme nice-org-html--temp-theme)
